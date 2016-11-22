15256e11d50e15eb6d43187c6f7bdd716233ebff
Hydra Su <hydragg@gmail.com>
2016-11-22 20:32:34 +0800

    update

--- app/index.js
+++ app/index.js
@@ -1,19 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  constructor(props) {
+
+const TodoList = (props) =>(
+  <ul>
+    {
+      props.items.map((item)=>(
+        <li key={item.id}>{item.text}</li>
+      ))
+    }
+  </ul>
+);
+
+class TodoListApp extends React.Component {
+  constructor(props){
     super(props);
+    this.onChange=this.onChange.bind(this);
+    this.onSubmit=this.onSubmit.bind(this);
     this.state = {
+      text:'',
+      items:[],
     };
   }
-  render() {
+  onChange(e){
+    this.setState({text:e.target.value});
+  }
+  onSubmit(e){
+    e.preventDefault();
+        const nextItems = this.state.items.concat([{text: this.state.text, id: Date.now()}]);
+        const nextText = '';
+        this.setState({items: nextItems, text: nextText});
+  }
+  render(){
     return (
       <div>
-        <h1>Hello, World!</h1>
+        <h3>TodoListApp</h3>
+        <TodoList items={this.state.items}/>
+        <form onSubmit={this.onSubmit}>
+          <input onChange={this.onChange} />
+          <button>Add#{this.state.items.length+1}</button>
+        </form>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <TodoListApp />,
+  document.getElementById("app")
+);
